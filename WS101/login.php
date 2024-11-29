e9aa1a3cf5d3d2bc4ba6a46aca6cbec2d499eb78
Joaquin0922 <joaquin.ampusta@gmail.com>
2024-11-29 19:53:40 +0800

    Add files via upload

---
+++ WS101/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Quezon City Express Login</title>
+</head>
+<body>
+    <div class="header">
+        <span>Do you need a business account?</span>
+        <a href="sign-up.php">SIGN UP NOW!</a>
+     
+    </div>
+    <div class="white"><img src="logo.png" alt=""></div>
+    <div class="login-container">
+        <div class="login-box">
+            <h1>Login to your account.</h1>
+            <p>Hello, Welcome to Quezon City University Express.</p>
+            <form>
+                <input type="text" placeholder="Username" required>
+                <input type="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+            </form>
+            <div>
+                <a href="#">Register</a> | <a href="#">Forgot Password?</a>
+            </div>
+        </div>
+
+        <div>
+            <img src="qcu-logo.png" alt="Quezon City University Logo" style="width: 300px;">
+        </div>
+    </div>
+
+    <div class="footer">
+        <div>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms and Conditions</a>
+        </div>
+        <div class="social-icons">
+            <span>Follow us on: </span>
+            <img src="ig.jpg" alt="Instagram">
+            <img src="fb.png" alt="Facebook">
+            <img src="x.jpg" alt="Twitter">
+        </div>
+    </div>
+</body>
+</html>
---
+++ WS101/sign-up.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Quezon City Express Sign Up</title>
+</head>
+<body>
+<div class="white"><img src="logo.png" alt=""></div>
+    <div class="header">
+        <span class="font">SIGN UP</span>
+     
+    </div>
+
+    <div class="login-container">
+        <div class="login-box">
+            <h1>Create a account.</h1>
+            <p>Hello, Welcome to Quezon City University Express.</p>
+            <form>
+                <input type="text" placeholder="Email" required>
+                <input type="password" placeholder="Username" required>
+                <input type="password" placeholder="Password" required>
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+
+        <div>
+            <img src="qcu-logo.png" alt="Quezon City University Logo" style="width: 300px;">
+        </div>
+    </div>
+
+    <div class="footer">
+        <div>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms and Conditions</a>
+        </div>
+        <div class="social-icons">
+            <span>Follow us on: </span>
+            <img src="ig.jpg" alt="Instagram">
+            <img src="fb.png" alt="Facebook">
+            <img src="x.jpg" alt="Twitter">
+        </div>
+    </div>
+</body>
+</html>
